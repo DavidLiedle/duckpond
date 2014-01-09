3944dc01adb836f1962a53601ea74a16d76547c8
David Christian Liedle <david.liedle@gmail.com>
2014-01-08 18:11:33 -0700

    More Skeletal Stuff
    Fleshing things out just a bit

---
+++ Java/DuckPond.java
@@ -0,0 +1,20 @@
+/**
+ * DuckPond
+ *
+ * @author David Christian Liedle <david.liedle@gmail.com>
+ * @link https://github.com/macdaddy/duckpond
+ *
+ * @license MIT Open Source License
+ *
+ */
+public class DuckPond {
+
+	public static void main( String args[] ){
+	
+		System.out.println();
+		System.out.println("DuckPond 0.0.1a");
+		System.out.println();
+		
+	} // End of public static void main( String args[] )
+	
+} // End of public class DuckPond
--- SHELL/duckpond.sh
+++ SHELL/duckpond.sh
@@ -16,11 +16,23 @@ echo -n "Select a payload: (Current active payload is '"
 # find out which payload is currently active
 echo -n "example-payload"
 echo "')"
+# Scan payloads folder
+# For each payload folder, ++numeric listing of payloads && echo the folder name as the payload name && echo out the meta.txt file for the description
 echo
 echo "1) First-Payload"
 echo "   First-Payload description, taken from its folder's meta.txt file"
 echo "2) Second-Payload"
 echo "   Second-Payload description, and so forth..."
+echo
+echo -n "Select a payload to activate: "
+# catch input
+# verify that the number chosen is a valid selection
+# compare to active payload
+# if not the same as the active payload:
+## move active payload back to its folder
+## move selected payload into active place
+### update marker to reflect which payload is the new active payload
+#### confirm to user and exit
 echo
 echo "END"
 echo
